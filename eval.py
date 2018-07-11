a003cb251dee848759b4bb10162fb92e103f7e17
coffeebenzene <ericteo_no1@hotmail.com>
2018-07-12 07:20:16 +0800

    eval script

--- eval.py
+++ eval.py
@@ -11,6 +11,38 @@ import dataloader
 import mappings
 import model
 
+def evaluate_accuracy(net_answers, gt_answers):
+    """Compute list of accuracies of all questions in minibatch.
+    
+    net_answers is the classification weights.
+    It is a tensor of shape (batch_size, net_num_answers)
+    net_num_answers is the number of classes the net classifies (should be 3001).
+    
+    gt_answers is the ground truths. There should be 10 ground truth answers for each queston.
+    It is a tensor of shape (batch_size, number_of_answers=10)
+    """
+    if len(net_answers) != len(gt_answers):
+        raise ValueError("Length of net and gt answers not equal! net:{} gt:{}"
+                         .format(len(net_answers), len(gt_answers)))
+    
+    # Get non-"<don't know>" predictions. <don't know> is index 0.
+    # Offset +1 to correct for slicing
+    net_predictions = net_answers[:,1:].argmax(dim=1)+1
+    
+    accs = []
+    for net_pred, gt_ans in zip(net_predictions, gt_answers):
+        net_pred = net_pred.item()
+        
+        acc_per_ans_subset = []
+        for i in range(len(gt_ans)):
+            ans_subset = torch.cat((gt_ans[:i], gt_ans[i+1:]))
+            num_matching = (ans_subset == net_pred).sum().item()
+            subset_acc = min(1, num_matching/3)
+            acc_per_ans_subset.append(subset_acc)
+        acc = sum(acc_per_ans_subset)/len(acc_per_ans_subset)
+        accs.append(acc)
+    return accs
+
 def unroll_ans(ans_idxs):
     ans_idxs = ans_idxs.numpy()
     # Unique answers for each question
@@ -55,47 +87,34 @@ def repeat(tensor, times):
     repeated = torch.cat(repeated, dim=0)
     return repeated
 
-def get_latest_state(model_path):
-    # Get .pth files only.
-    # Should contain mixture of model#.pth and optimizer#.pth
-    modelfiles = os.listdir(model_path)
-    modelfiles = (os.path.splitext(f) for f in modelfiles)
-    modelfiles = {name for name,ext in modelfiles if ext==".pth"}
+def get_models(model_path):
+    """Returns iterable of (filename, model state_dict) tuples"""
+    # model_path is a .pth file. load and return it.
+    if os.path.splitext(model_path)[1]==".pth":
+        model_sd = torch.load(model_path, map_location='cpu')
+        return [(model_path, model_sd)]
     
-    largest_epoch = -1
-    for name in modelfiles:
-        if name.startswith("model"):
-            epoch_num = name[5:]
-            # Check that epoch number is valid
-            try:
-                epoch_num = int(epoch_num)
-            except ValueError:
-                continue
-            # Check that optimizer state exists
-            if "optimizer{}".format(epoch_num) not in modelfiles:
-                continue
-            if epoch_num > largest_epoch:
-                largest_epoch = epoch_num
-    
-    # Load and return state_dict of mode, optimizer and scheduler
-    if largest_epoch == -1:
-        return None, None, None, largest_epoch
-    model_file = os.path.join(model_path,"model{}.pth".format(largest_epoch))
-    optimizer_file = os.path.join(model_path,"optimizer{}.pth".format(largest_epoch))
-    model_sd = torch.load(model_file, map_location='cpu')
-    optimizer_sd, scheduler_sd = torch.load(optimizer_file, map_location='cpu')
-    return model_sd, optimizer_sd, scheduler_sd, largest_epoch
+    # model_path points to folder, load modules lazily as generator.
+    files = os.listdir(model_path)
+    files = (os.path.splitext(f) for f in files)
+    modelfiles = []
+    for name, ext in files:
+        if ext==".pth" and name.startswith("model"):
+            modelfiles.append(name+ext)
+    def module_gen():
+        for m_filename in modelfiles:
+            model_file = os.path.join(model_path, m_filename)
+            model_sd = torch.load(model_file, map_location='cpu')
+            yield (m_filename, model_sd)
+    return module_gen()
 
 
 
 def main(args):
     device = torch.device(args.device)
-    # Model will be saved into directory at every epoch
-    if not os.path.exists(args.model_path):
-        os.makedirs(args.model_path)
-    # Load the latest states if they exist.
-    latest_state = get_latest_state(args.model_path)
-    model_sd, optimizer_sd, scheduler_sd, last_epoch = latest_state
+    
+    # Get models
+    model_sd_iter = get_models(args.model_path)
     
     # mapping wrappers
     with open(args.vocab_path,"rb") as f:
@@ -108,9 +127,8 @@ def main(args):
                                         args.annotation_path,
                                         args.question_path,
                                         vocab, answers,
-                                        #batch_size=8, shuffle=False # DEBUG
                                         batch_size=args.batch_size,
-                                        shuffle=True,
+                                        shuffle=False,
                                         num_workers=args.num_workers
                                        )
     
@@ -118,31 +136,26 @@ def main(args):
     # Note: Expected values: len(vocab) = 8254, len(answers) = 3001
     net = model.VQAnet(len(vocab), embedding_size=128, lstm_size=512,
                        fc_size=1024, num_answers=len(answers))
-    if model_sd is not None:
-        net.load_state_dict(model_sd)
-    net.to(device)
-    net.train()
     
+    # Compute loss for validation too.
     # Don't reduce loss, so can manually weight by answers.
     criterion = nn.CrossEntropyLoss(reduce=False)
-    # Note: parameters of optimizer are actually defaults.
-    optimizer = torch.optim.Adam(net.trainable_parameters(), lr=0.001, betas=(0.9, 0.999))
-    if optimizer_sd is not None:
-        optimizer.load_state_dict(optimizer_sd)
-    # Will likely never reach 12 epochs, but appoximately follows what paper uses
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 12, 0.5)
-    if scheduler_sd is not None:
-        scheduler.load_state_dict(scheduler_sd)
     
-    # Free up memory
-    del model_sd, optimizer_sd, scheduler_sd
-    
-    iterations_per_epoch = len(data_loader)
-    for epoch in range(last_epoch+1, last_epoch+1+args.num_epochs):
-        scheduler.step()
-        epoch_start = time.time()
+    # Compute validation for each model.
+    stats = {}
+    iterations = len(data_loader)
+    for model_filename, model_sd in model_sd_iter:
+        net.load_state_dict(model_sd)
+        net.to(device)
+        net.eval()
+        
+        
+        start = time.time()
         total_loss = 0
         total_weight = 0
+        total_acc = 0
+        total_count = 0
+        
         for i, (images, qn_idxs, ans_idxs) in enumerate(data_loader,1):
             images = images.to(device)
             if device.type=="cuda":
@@ -153,66 +166,69 @@ def main(args):
             ans_unique = ans_unique.to(device)
             ans_weights = ans_weights.type(torch.float).to(device)
             
+            output = net(images, qn_idxs)
+            
+            # Compute loss
             if sum(ans_repeats) != 0:
-                net.zero_grad()
-                
-                output = net(images, qn_idxs)
-                output = repeat(output, ans_repeats)
-                losses = criterion(output, ans_unique)
+                repeated_output = repeat(output, ans_repeats)
+                losses = criterion(repeated_output, ans_unique)
                 sum_losses = (losses*ans_weights).sum()
                 sum_weights = ans_weights.sum()
                 loss = sum_losses / sum_weights
                 
-                loss.backward()
-                optimizer.step()
                 total_loss += sum_losses.item()
                 total_weight += sum_weights.item()
             
+            # Compute accuracy
+            accs = evaluate_accuracy(output, ans_idxs)
+            total_acc += sum(accs)
+            total_count += len(accs)
+            
             if i%args.log_period == 0:
-                current_time = time.time() - epoch_start
+                current_time = time.time() - start
                 current_avg_loss = total_loss/total_weight
-                print("Epoch {:2}, Step: {:6}/{:6}, Loss:{:.5f}, time:{:.3f}s".format(
-                       epoch, i, iterations_per_epoch, current_avg_loss, current_time),
-                       flush=True, end="\r"
+                current_avg_acc = total_acc/total_count
+                print("Model: {:11}, Step: {:6}/{:6}, Loss:{:.5f}, Acc:{:.5f}, time:{:.3f}s".format(
+                      model_filename, i, iterations, current_avg_loss, current_avg_acc, current_time),
+                      flush=True, end="\r"
                      )
         
-        current_time = time.time() - epoch_start
+        current_time = time.time() - start
         current_avg_loss = total_loss/total_weight
-        print("Epoch {:2}, Step: {:6}/{:6}, Loss:{:.5f}, time:{:.3f}s".format(
-                       epoch, i, iterations_per_epoch, current_avg_loss, current_time),
-                       flush=True
-                     )
-        
-        # Save model
-        model_file = os.path.join(args.model_path,"model{}.pth".format(epoch))
-        optimizer_file = os.path.join(args.model_path,"optimizer{}.pth".format(epoch))
-        torch.save(net.state_dict(), model_file)
-        torch.save((optimizer.state_dict(),scheduler.state_dict()), optimizer_file)
+        current_avg_acc = total_acc/total_count
+        print("Model: {:11}, Step: {:6}/{:6}, Loss:{:.5f}, Acc:{:.5f}, time:{:.3f}s".format(
+              model_filename, i, iterations, current_avg_loss, current_avg_acc, current_time),
+              flush=True
+             )
+        stats[model_filename] = (current_avg_loss, current_avg_acc)
+    print(stats)
+    with open(args.stats_path, "wb") as f:
+        pickle.dump(stats, f)
+    return stats
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', type=str, default='models/' , help='path for saving trained models')
+    parser.add_argument('--stats_path', type=str, default='stats.pkl' , help="path to save dict of evaluation stats.")
+    parser.add_argument('--model_path', type=str, default='models/' , help="path for trained model. Can be either a folder or a .pth file containing model's state_dict.")
     parser.add_argument('--vocab_path', type=str, default='vocab.pkl', help='path for vocabulary wrapper')
     parser.add_argument('--ans_path', type=str, default='answers.pkl', help='path for answers wrapper')
-    parser.add_argument('--image_dir', type=str, default='images/train2014', help='directory for images')
+    parser.add_argument('--image_dir', type=str, default='images/val2014', help='directory for images')
     parser.add_argument('--annotation_path', type=str,
-                        default='annotations/v2_mscoco_train2014_annotations.json',
+                        default='annotations/v2_mscoco_val2014_annotations.json',
                         help='path for annotations json')
     parser.add_argument('--question_path', type=str,
-                        default='questions/v2_OpenEnded_mscoco_train2014_questions.json',
+                        default='questions/v2_OpenEnded_mscoco_val2014_questions.json',
                         help='path for questions json')
     parser.add_argument('--device', type=str, default="auto", choices=("auto","cpu","cuda"), help='device to use')
     parser.add_argument('--log_period', type=int, default=100,
                         help='Number of minibatches before progress is updated')
-    parser.add_argument('--num_epochs', type=int, default=3, help='Number of epochs to train for')
     parser.add_argument('--batch_size', type=int, default=128, help='Batch size')
-    # Note: batch_size 8 seems to consume 1GB of GPU memory(?)
     parser.add_argument('--num_workers', type=int, default=0, help='Number of workers for dataloader')
     # Note: seems like on windows, each worker uses about 2GB RAM.
     # But on ubuntu, they can apparently share memory and consume much less.
-    # 32 seems ok on ubuntu (maybe 8/16 is better?)
+    # 8 seems ok on ubuntu
     
     args = parser.parse_args()
     
@@ -220,4 +236,4 @@ if __name__ == '__main__':
         args.device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using device: {}".format(args.device), flush=True)
     
-    main(args)
+    stats = main(args)
